b3387a190f0f485ca58c7d4bb3d88b8362b11205
Kaloomae54 <kaloomae54@gmail.com>
2025-05-30 18:31:15 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello depuis Bernard Serveur !"
+    return "Serveur Bernard Saute en ligne 🚀"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render fournit automatiquement la variable PORT
+    app.run(host="0.0.0.0", port=port)        # Très important pour que Render détecte le port
